a3c5becc7f445a03d2218cb7a47a9fc2d506840d
Ledger_MacMini <laored@outlook.com>
2026-01-02 23:39:37 +0800

    feat: Initialize etLLM iOS application with core UI, assets, and project configuration.feat: Initialize etLLM iOS application with core UI, assets, and project configuration.

---
+++ apple/Application/Message.swift
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import UIKit
+
+enum MessageType {
+  case prompted
+  case llamagenerated
+  case qwengenerated
+  case phi4generated
+  case gemma3generated
+  case llavagenerated
+  case smollm3generated
+  case voxtralgenerated
+  case info
+}
+
+struct Message: Identifiable, Equatable {
+  let id = UUID()
+  let dateCreated = Date()
+  var dateUpdated = Date()
+  var type: MessageType = .prompted
+  var text = ""
+  var tokenCount = 0
+  var image: UIImage?
+  var tokensPerSecond: Double = 0
+  var lastSpeedMeasurementAt: Date?
+}
